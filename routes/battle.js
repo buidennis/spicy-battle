d6b287a645e7f4ed1f344c7c4af7f714d9003a07
Thuc Phan <tcp004@ucsd.edu>
2020-02-13 17:36:06 -0800

    switched data from battle.js to json file

--- routes/battle.js
+++ routes/battle.js
@@ -16,38 +16,7 @@
 /*
  * GET home page.
  */
-
+var data = require('../data.json');
 exports.view = function(req, res){
-  res.render('battle', {
-     'exercises': [
-       { 'exerciseTitle': 'Pushups',
-         'exerciseName': 'Pushups',
-         'repetitions:': '10',
-         'calories:': '25',
-         'video': '/videos/pushups.mp4',
-         'id': 'exercise1'
-       },
-       { 'exerciseTitle': 'Situps',
-         'exerciseName': 'Situps',
-         'repetitions:': '15',
-         'calories:': '20',
-         'video': '/videos/situps.mp4',
-         'id': 'exercise2'
-       },
-       { 'exerciseTitle': 'Jumping Jacks',
-         'exerciseName': 'Jumpingjacks',
-         'repetitions:': '30',
-         'calories:': '10',
-         'video': '/videos/jumpingjacks.mp4',
-         'id': 'excercise3'
-       },
-       { 'exerciseTitle': 'Squats',
-         'exerciseName': 'Squats',
-         'repetitions:': '20',
-         'calories:': '15',
-         'video': '/videos/squats.mp4',
-         'id': 'exercise4'
-       }
-     ]
-   });
+  res.render('battle', data);
  };
--- routes/exercise.js
+++ routes/exercise.js
@@ -2,14 +2,10 @@
 /*
  * GET home page.
  */
-
+var data = require('../data.json');
 exports.view = function(req, res){
   var name = req.params.name;
   console.log("Exercise name is: " + name);
-  console.log("/videos/" + name + ".mp4")
   console.log("in exercise");
-    res.render('exercise', {
-      'exerciseName': name,
-      'instructionVideo': '/videos/'+ name +'.mp4'
-    });
+  res.render('exercise', data);
 };
