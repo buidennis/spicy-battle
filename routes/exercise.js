801012adc2fa52d40cecf08a4d192f1f795c6e18
Thuc Phan <tcp004@ucsd.edu>
2020-02-13 19:48:51 -0800

    reverted changes. *working*

--- routes/exercise.js
+++ routes/exercise.js
@@ -6,6 +6,10 @@ var data = require('../data.json');
 exports.view = function(req, res){
   var name = req.params.name;
   console.log("Exercise name is: " + name);
+  console.log("/videos/" + name + ".mp4")
   console.log("in exercise");
-  res.render('exercise', data);
+    res.render('exercise', {
+      'exerciseName': name,
+      'instructionVideo': '/videos/'+ name +'.mp4'
+    });
 };
