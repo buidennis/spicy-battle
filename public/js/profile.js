da3e09cb586417561e7e9daf825cb26088f32b46
Dennit <Dennit@users.noreply.github.com>
2020-02-21 10:11:39 -0800

    Update profile.js

--- public/js/profile.js
+++ public/js/profile.js
@@ -9,6 +9,23 @@ function initializePage() {
     console.log("Javascript connected!");
     $.get('/index1', callbackFunc);
 }
+function changeHeight( variable){
+  console.log("Change height");
+	$.get("/changeHeight/", callbackFunc);
+}
+function changeWeight( variable){
+  console.log("Change weight");
+	$.get("/changeWeight/", callbackFunc);
+}
+function changeAge( variable){
+  console.log("Change age");
+	$.get("/changeAge/", callbackFunc);
+}
+function changeSex( variable){
+  console.log("Change sex");
+	$.get("/changeSex/", callbackFunc);
+}
+
 
 function callbackFunc(request,response){
     $("#name").text(request.info.name);
